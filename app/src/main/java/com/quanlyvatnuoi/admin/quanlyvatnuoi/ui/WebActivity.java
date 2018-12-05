e32e356c305d10da731a0d7a8d9f1f289e3bfb89
duynam98 <duynam6262@gmail.com>
2018-12-05 21:51:20 +0700

    Commit happy

--- app/src/main/java/com/quanlyvatnuoi/admin/quanlyvatnuoi/ui/WebActivity.java
+++ app/src/main/java/com/quanlyvatnuoi/admin/quanlyvatnuoi/ui/WebActivity.java
@@ -1,6 +1,7 @@
 package com.quanlyvatnuoi.admin.quanlyvatnuoi.ui;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,6 +17,9 @@ public class WebActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
         initView();
+        setTitle("Thông tin");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         webview.setWebViewClient(new WebViewClient());
         webview.loadUrl("http://nguoichannuoi.vn/");
     }
